41da2470fe5fa88e9e7272391a43436e8614aadd
Stefan Ducic <ducic_stefan@hotmail.com>
2019-12-22 14:30:11 +0100

    delete all

--- reloadLog.php
+++
@@ -1,8 +0,0 @@
-<?php
-    
-        echo "Uspesno ste registrovali nalog";
-        echo "<br>";
-        echo  "Stranica za logovanje ce automatski biti prikazana, sacekajte.";
-        header( "refresh:3;url=index.php" );
-    
-?>
