83b3ed06d4d6468eea520adf8407726656bd9ecb
Stefan Ducic <ducic_stefan@hotmail.com>
2019-12-22 14:25:16 +0100

    commit

---
+++ reloadLog.php
@@ -0,0 +1,8 @@
+<?php
+    
+        echo "Uspesno ste registrovali nalog";
+        echo "<br>";
+        echo  "Stranica za logovanje ce automatski biti prikazana, sacekajte.";
+        header( "refresh:3;url=index.php" );
+    
+?>
